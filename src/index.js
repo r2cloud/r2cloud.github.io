10176a192f6d455dfc2407e5a5c51c373066d0fc
r2cloud <alkali+r2cloud@alkalinesecurity.com>
2022-11-05 21:33:27 -0400

    use optimized wasm-terminal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import WasmTerminal, { fetchCommandFromWAPM } from "@wasmer/wasm-terminal";
 import { lowerI64Imports } from "@wasmer/wasm-transformer";
-import { WasmFs, Promise } from "@wasmer/wasmfs";
+import { WasmFs } from "@wasmer/wasmfs";
 
 const wasmFs = new WasmFs();
 wasmFs.fs.mkdir("/tmp", ()=>{});
+// wasmFs.fs.writeFileSync(".radare2rc", "eco darkda\ne scr.utf8=true");
 const cache = {};
 
 // Let's write handler for the fetchCommand property of the WasmTerminal Config.
@@ -11,22 +12,37 @@ const fetchCommandHandler = async ({args, env}) => {
   let commandName = args[0];
   // Let's return a "CallbackCommand" if our command matches a special name
   if (commandName === "callback-command") {
-    const callbackCommand = async (options, wasmFs) => {
+    return async (options, wasmFs) => {
       return `Callback Command Working! Options: ${options}, fs: ${wasmFs}`;
     };
-    return callbackCommand;
   }
 
-  if (!cache[args[0]]) {
+  if (commandName === "cat") {
+    return async (options, wasmFs) => {
+      return wasmFs.fs.readFileSync(args[1]).toString();
+    };
+  }
+
+  if (commandName === "ls") {
+    return async (options, wasmFs) => {
+      let dir = args[1];
+      if (!dir) {
+        dir = ".";
+      }
+      return wasmFs.fs.readdirSync(dir).join("\n");
+    };
+  }
+
+  if (!cache[commandName]) {
     // Let's fetch a wasm Binary from WAPM for the command name.
     const wasmBinary = await fetchCommandFromWAPM({args});
 
     // lower i64 imports from Wasi Modules, so that most Wasi modules
     // Can run in a Javascript context.
-    cache[args[0]] = await lowerI64Imports(wasmBinary);
-    return cache[args[0]];
+    cache[commandName] = await lowerI64Imports(wasmBinary);
+    return cache[commandName];
   } else {
-    return cache[args[0]];
+    return cache[commandName];
   }
 };
 
@@ -34,7 +50,8 @@ const fetchCommandHandler = async ({args, env}) => {
 const wasmTerminal = new WasmTerminal({
   // Function that is run whenever a command is fetched
   fetchCommand: fetchCommandHandler,
-  wasmFs: wasmFs
+  wasmFs: wasmFs,
+  processWorkerUrl: "process.worker.js"
 });
 
 // Let's print out our initial message
